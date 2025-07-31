69c5f2346f8d451825c990d9b592e70e986a91de
Sam Alex Koshy <96406256+TheParadox543@users.noreply.github.com>
2025-07-31 09:50:29 +0530

    refactored data models

---
+++ composeApp/src/commonMain/kotlin/com/paradox543/malankaraorthodoxliturgica/data/model/PageNode.kt
@@ -0,0 +1,12 @@
+package com.paradox543.malankaraorthodoxliturgica.data.model
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class PageNode(
+    val route: String,
+    val filename: String? = null,
+    val parent: String?,
+    val children: List<PageNode> = emptyList(),
+    val languages: List<String> = listOf(AppLanguage.MALAYALAM.code),
+)
